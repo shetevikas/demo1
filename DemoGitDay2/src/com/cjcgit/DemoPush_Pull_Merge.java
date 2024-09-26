51bca8a199c736b791447a98de49101890e0212d
shetevikas <shetevikas2006@gmail.com>
2024-09-26 11:33:02 +0530

    Update DemoPush_Pull_Merge.java

--- DemoGitDay2/src/com/cjcgit/DemoPush_Pull_Merge.java
+++ DemoGitDay2/src/com/cjcgit/DemoPush_Pull_Merge.java
@@ -10,4 +10,12 @@ public void m1()
 {
 	System.out.println("m1 method");
 }
+	public void m2()
+{
+	System.out.println("m2 method");
+}
+		public void m2()
+{
+	System.out.println("m2 method");
+}
 }
